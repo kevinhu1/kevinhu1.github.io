1708d3f5aa510f015fee88552d1cf9f02c1b1ae5
kevinhu1 <kevinhu@hawaii.edu>
2014-06-07 10:36:48 -1000

    hover over image

--- javascripts/swappics.js
+++ javascripts/swappics.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 function MM_swapImgRestore() { //v3.0
 var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
 }
@@ -20,4 +19,19 @@ function MM_swapImage() { //v3.0
 var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
 if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
 }
-</script>
+
+
+
+
+
+$(document).ready(function(){
+ 
+$("img.a").hover(
+function() {
+$(this).stop().animate({"opacity": "0"}, "medium");
+},
+function() {
+$(this).stop().animate({"opacity": "1"}, "medium");
+});
+ 
+});
